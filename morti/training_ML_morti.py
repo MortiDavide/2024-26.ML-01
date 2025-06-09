09bd5030283844162f5316ab76d52e8b30c00b79
MortiDavide <morti.davide@gmail.com>
2025-06-09 13:30:57 +0200

    commit finale (si spera)

--- morti/training_ML_morti.py
+++ morti/training_ML_morti.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error
 
 df = pd.read_csv("house_price_regression_dataset.csv")
 
@@ -18,11 +17,8 @@ print(len(x_train), len(x_test), len(y_train), len(y_test))
 
 linreg = LinearRegression()
 
-linreg.fit(x_train, y_train)
+import joblib
 
-y_pred = linreg.predict(x_test)
+pipe_fittato = linreg.fit(x_train, y_train)
 
-mae = mean_absolute_error(y_pred, y_test)
-mape = mean_absolute_percentage_error(y_pred, y_test)
-
-print(mae, mape)
+joblib.dump(pipe_fittato, "linear_model.joblib")
